556869449f086953120373d61dc8040ab4a5e176
xzxin <xiazhenxin123@126.com>
2021-02-11 21:41:01 +0800

    用户登录和校验接口

--- src/main/java/com/mmall/controller/portal/UserController.java
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,5 +1,6 @@
 package com.mmall.controller.portal;
 
+import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
@@ -28,6 +29,25 @@ public class UserController {
       session.setAttribute(CURRENT_USER, response.getData());
     }
     return response;
+  }
+  
+  @RequestMapping(value = "logout.do", method = RequestMethod.GET)
+  @ResponseBody
+  public ServerResponse<String> logout(HttpSession session) {
+    session.removeAttribute(CURRENT_USER);
+    return ServerResponse.createdBySuccess();
+  }
+  
+  @RequestMapping(value = "register.do", method = RequestMethod.GET)
+  @ResponseBody
+  public ServerResponse<String> register(User user) {
+    return iUserService.register(user);
+  }
+  
+  @RequestMapping(value = "check_valid.do", method = RequestMethod.GET)
+  @ResponseBody
+  public ServerResponse<String> checkValid(String str, String type) {
+    return iUserService.checkValid(str, type);
   }
 }
 
