b672b60093f1b21c6d5845167f296b1a338f4314
xzxin <xiazhenxin123@126.com>
2021-02-13 11:13:38 +0800

    后台品类管理功能开发

---
+++ src/main/java/com/mmall/service/Impl/CategoryServiceImpl.java
@@ -0,0 +1,93 @@
+package com.mmall.service.Impl;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.CategoryMapper;
+import com.mmall.pojo.Category;
+import com.mmall.service.ICategoryService;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @ClassName: CategoryServiceImpl
+ * @Description: 分类接口实现类
+ * @Author
+ * @Date 2021/2/13
+ * @Version 1.0
+ */
+@Service("iCategoryService")
+public class CategoryServiceImpl implements ICategoryService {
+  private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+  @Autowired
+  private CategoryMapper categoryMapper;
+  
+  public ServerResponse addCategory(String categoryName, Integer parentId) {
+    if (parentId == null || StringUtils.isBlank(categoryName)) {
+      return ServerResponse.createByErrorMessage("添加品类参数错误");
+    }
+    Category category = new Category();
+    category.setName(categoryName);
+    category.setParentId(parentId);
+    category.setStatus(true); // 分类可用
+  
+    int rowCnt = categoryMapper.insert(category);
+    if (rowCnt > 0) {
+      return ServerResponse.createBySuccessMessage("添加品类成功");
+    }
+    return ServerResponse.createByErrorMessage("添加品类失败");
+  }
+  
+  public ServerResponse updateCategoryName(Integer categoryId, String categoryName) {
+    if (categoryId == null || StringUtils.isBlank(categoryName)) {
+      return ServerResponse.createByErrorMessage("更新品类参数错误");
+    }
+    Category category = new Category();
+    category.setId(categoryId);
+    category.setName(categoryName);
+  
+    int rowCount = categoryMapper.updateByPrimaryKeySelective(category);
+    if (rowCount > 0) {
+      return ServerResponse.createBySuccessMessage("更新品类名称成功");
+    }
+    return ServerResponse.createByErrorMessage("更新品类失败");
+  }
+  
+  public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId) {
+    List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+    if (CollectionUtils.isEmpty(categoryList)) {
+      logger.info("未找到当前分类的子分类");
+    }
+    return ServerResponse.createBySuccess(categoryList);
+  }
+  
+  public ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId) {
+    HashSet<Category> categorySet = Sets.newHashSet();
+    findChildCategory(categorySet, categoryId);
+    List<Integer> categoryIdList = Lists.newArrayList();
+    for (Category categoryItem : categorySet) {
+      categoryIdList.add(categoryItem.getId());
+    }
+    return ServerResponse.createBySuccess(categoryIdList);
+  }
+  
+  private void findChildCategory(Set<Category> categorySet, int categoryId) {
+    Category category = categoryMapper.selectByPrimaryKey(categoryId);
+    if (category != null) {
+      categorySet.add(category);
+    }
+    List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+    for (Category categoryItem : categoryList) {
+      findChildCategory(categorySet, categoryItem.getId());
+    }
+  }
+}
--- src/main/java/com/mmall/service/Impl/UserServiceImpl.java
+++ src/main/java/com/mmall/service/Impl/UserServiceImpl.java
@@ -175,4 +175,12 @@ public class UserServiceImpl implements IUserService {
     user.setPassword(StringUtils.EMPTY);
     return ServerResponse.createBySuccess(user);
   }
+  
+  // backend
+  public ServerResponse checkAdminRole(User user) {
+    if (user != null && user.getRole() == Const.Role.ROLE_ADMIN) {
+      return ServerResponse.createBySuccess();
+    }
+    return ServerResponse.createByError();
+  }
 }
