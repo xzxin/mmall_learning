b316979f8eb7e9a7ca6cbfa3b5641793da8ff248
xzxin <xiazhenxin123@126.com>
2021-02-16 19:06:42 +0800

    对接支付模块代码

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -54,4 +54,73 @@ public class Const {
       this.code = code;
     }
   }
+  
+  public enum OrderStatusEnum {
+    CANCLED(0, "已取消"),
+    NO_PAY(10, "未支付"),
+    PAID(20, "已支付"),
+    SHIPPED(40, "已发货"),
+    ORDER_SUCCESS(50, "订单完成"),
+    ORDER_CLOSE(60, "订单关闭");
+    
+    private String value;
+    private int code;
+    
+    OrderStatusEnum(int code, String value) {
+      this.value = value;
+      this.code = code;
+    }
+  
+    public String getValue() {
+      return value;
+    }
+  
+    public void setValue(String value) {
+      this.value = value;
+    }
+  
+    public int getCode() {
+      return code;
+    }
+  
+    public void setCode(int code) {
+      this.code = code;
+    }
+  }
+  
+  public interface AlipayCallback {
+    String TRADE_STATUS_WAIT_BUYER_PAY = "WAIT_BUYER_PAY";
+    String TRADE_STATUS_TRADE_SUCCESS = "TRADE_SUCCESS";
+    
+    String RESPONSE_SUCCESS = "success";
+    String RESPONSE_FAILED = "failed";
+  }
+  
+  public enum PayPlatformEnum {
+    ALIPAI(1, "支付宝");
+    private String value;
+  
+    PayPlatformEnum(int code, String value) {
+      this.value = value;
+      this.code = code;
+    }
+  
+    private int code;
+  
+    public String getValue() {
+      return value;
+    }
+  
+    public void setValue(String value) {
+      this.value = value;
+    }
+  
+    public int getCode() {
+      return code;
+    }
+  
+    public void setCode(int code) {
+      this.code = code;
+    }
+  }
 }
